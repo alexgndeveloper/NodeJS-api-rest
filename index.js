c75dc3abd469ec3a28cb8df5f4ec27b91186e96b
Alejandro Garcia <alexgn26developer@gmail.com>
2022-03-15 00:17:20 +0100

    Endpoints GET, POST, PUT and DELETE

--- index.js
+++ index.js
@@ -9,6 +9,21 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/api/product", (req, res) => {
+  res.send(200, { products: [] });
+});
+
+app.get("/api/product/:productId", (req, res) => {});
+
+app.post("/api/product", (req, res) => {
+  console.log(req.body);
+  res.status(200).send({ message: "El producto se ha recibido" });
+});
+
+app.put("/api/product/:productId", (req, res) => {});
+
+app.delete("/api/product/:productId", (req, res) => {});
+
 app.listen(port, () => {
   console.log(`API REST corriendo en http://localhost:${port}`);
 });
