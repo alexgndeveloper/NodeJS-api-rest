c7ceaf6137580ac898d40f64b4c91ea5ebd7a73e
alexgndeveloper <alexgn26developer@gmail.com>
2022-03-18 20:36:05 +0100

    Constant creation

--- index.js
+++ index.js
@@ -10,22 +10,28 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/api/product", (req, res) => {
+
+// URL API
+const PRODUCT = "/api/product";
+const PRODUCT_PRODUCT_ID = "/api/product/:productId";
+const CONEXION_MONDO_DB = "mongodb://localhost:27017/shop";
+
+app.get(PRODUCT, (req, res) => {
   res.send(200, { products: [] });
 });
 
-app.get("/api/product/:productId", (req, res) => {});
+app.get(PRODUCT_PRODUCT_ID, (req, res) => { });
 
-app.post("/api/product", (req, res) => {
+app.post(PRODUCT, (req, res) => {
   console.log(req.body);
   res.status(200).send({ message: "El producto se ha recibido" });
 });
 
-app.put("/api/product/:productId", (req, res) => {});
+app.put(PRODUCT_PRODUCT_ID, (req, res) => { });
 
-app.delete("/api/product/:productId", (req, res) => {});
+app.delete(PRODUCT_PRODUCT_ID, (req, res) => { });
 
-mongoose.connect("mongodb://localhost:27017/shop", (err, res) => {
+mongoose.connect(CONEXION_MONDO_DB, (err, res) => {
   if (err) {
     return console.log(`Error al conectar a la base de datos ${err}`);
   }
