fec76c8137c5f63a21debd2afad0cf5d7828483c
alexgndeveloper <alexgn26developer@gmail.com>
2022-06-07 16:46:29 +0200

    Method get all product and get product

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const Product = require("./models/product");
+const product = require("./models/product");
 
 const app = express();
 const port = process.env.PORT || 3001;
@@ -11,22 +12,42 @@ const port = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
 // URL API
 const PRODUCT = "/api/product";
 const PRODUCT_PRODUCT_ID = "/api/product/:productId";
 const CONEXION_MONDO_DB = "mongodb://localhost:27017/shop";
 
 app.get(PRODUCT, (req, res) => {
-  res.status(200).send({ products: [] });
+  Product.find({}, (err, products) => {
+    if (err) {
+      res.status(500).send(`Error al realizar la petición ${err}`);
+    }
+
+    if (!product) {
+      res.status(404).send('No existen productos');
+    }
+
+    res.status(200).send({ products });
+  });
 });
 
-app.get(PRODUCT_PRODUCT_ID, (req, res) => { });
+app.get(PRODUCT_PRODUCT_ID, (req, res) => {
+  let productId = req.params.productId;
+
+  Product.findById(productId, (err, product) => {
+    if (err) {
+      res.status(500).send(`Error al realizar la petición ${err}`);
+    }
+
+    if (!product) {
+      res.status(404).send('El producto no existe');
+    }
+
+    res.status(200).send({ product });
+  });
+});
 
 app.post(PRODUCT, (req, res) => {
-  console.log(`POST ${PRODUCT}`);
-  console.log(req.body);
-
   let product = new Product();
   product.name = req.body.name;
   product.picture = req.body.picture;
@@ -36,12 +57,11 @@ app.post(PRODUCT, (req, res) => {
 
   product.save((err, productStored) => {
     if (err) {
-      res.status(500).send(`Error al salvar en la base de datos ${err}`)
+      res.status(500).send(`Error al salvar en la base de datos ${err}`);
     }
 
     res.status(200).send({ product: productStored });
   });
-
 });
 
 app.put(PRODUCT_PRODUCT_ID, (req, res) => { });
